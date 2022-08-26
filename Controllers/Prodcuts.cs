660ee009b6412bf9cfc0280bc6bf649630a896a1
mvkrishnas <36788323+mvkrishnas@users.noreply.github.com>
2022-08-26 21:32:41 +0530

    Add project files.

---
+++ Controllers/Prodcuts.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Prodcuts : ControllerBase
+    {
+        private static readonly List<string> productItems = new();
+        private readonly ILogger<Prodcuts> logger;
+        public Prodcuts(ILogger<Prodcuts> _logger)
+        {
+            logger = _logger;
+        }
+        [HttpGet]
+        public IEnumerable<string> GetProdcut()
+        {
+            return productItems;
+        }
+        [HttpPost]
+        public string AddProdcut(string productName)
+        {
+            string result = $"{productName} added into prodcut list";
+            if (!productItems.Contains(productName))
+                productItems.Add(productName);
+            else
+                result = $"{productName} already in prodcut list";
+            return result;
+        }
+        [HttpDelete]
+        public string DeleteProdcut(string productName)
+        {
+            string result = $"{productName} deleted from prodcut list";
+            if (productItems.Contains(productName))
+                productItems.Remove(productName);
+            else
+                result = $"{productName} not avaliable in prodcut list";
+            return result;
+        }
+    }
+}
